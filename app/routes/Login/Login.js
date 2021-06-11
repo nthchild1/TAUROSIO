5ede20fe51bac18493e4b5c46cc3466433d20505
TKY2048 <tky2048@gmail.com>
2021-06-11 17:18:50 -0500

    Login screen draft

--- app/routes/Login/Login.js
+++ app/routes/Login/Login.js
@@ -1,12 +1,94 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import client from '../../../src/client';
 
-const Login = props => {
+const styles = StyleSheet.create({
+  AppContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3c3c3c',
+  },
+  TextInput: {
+    backgroundColor: '#4f4f4f',
+    width: '90%',
+    margin: '5%',
+  },
+});
+
+function Login({handleLogin}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.AppContainer}>
+      <Text style={{color: 'white'}}>TAUROS</Text>
+      {isLoading && <ActivityIndicator size={'large'} color={'green'} />}
+      <TextInput
+        value={email}
+        placeholder={'email'}
+        style={styles.TextInput}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        value={password}
+        placeholder={'password'}
+        style={styles.TextInput}
+        onChangeText={setPassword}
+      />
+      <Button
+        title={'Iniciar Sesión'}
+        onPress={async () => {
+          setIsLoading(true);
+          const {success} = await handleLogin(email, password);
+          setIsLoading(false);
+
+          if (success) {
+            console.log('success!');
+          }
+        }}
+      />
     </View>
   );
+}
+
+Login.defaultProps = {
+  handleLogin: (email, password) => {
+    return client
+      .post(
+        '/api/v3/auth/signin/',
+        {
+          email,
+          password,
+          device_name: 'iphone 11',
+          unique_device_id: 'TKY2048',
+        },
+        {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      )
+      .then(({data: {payload}}) => {
+        const {access} = payload;
+        client.interceptors.request.use(function (config) {
+          config.headers.Authorization = `JWT-V3 ${access}`;
+          return config;
+        });
+        return {success: true};
+      })
+      .catch(e => {
+        console.log(e.toJSON());
+        return {success: false};
+      });
+  },
 };
 
 export default Login;
--- src/client.js
+++ src/client.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const client = (() =>
   axios.create({
-    baseURL:
-      'https://gist.github.com/jeezascodes/3c65a75c9772c66692ccea4a6ea7a9b7',
+    baseURL: 'https://api.staging.tauros.io',
     timeout: 30000,
   }))();
 
